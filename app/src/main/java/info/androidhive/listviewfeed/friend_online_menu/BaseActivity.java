6fe6e954be02d1c1434b5f3d8e6ec5816bba6207
Dao Quoc Viet <daoquocviet11i3@gmail.com>
2015-07-17 15:33:30 +0700

    Sua

--- app/src/main/java/info/androidhive/listviewfeed/friend_online_menu/BaseActivity.java
+++ app/src/main/java/info/androidhive/listviewfeed/friend_online_menu/BaseActivity.java
@@ -20,7 +20,7 @@ public class BaseActivity extends SlidingFragmentActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+//Sai
 		setTitle(mTitleRes);
 
 		// set the Behind View
